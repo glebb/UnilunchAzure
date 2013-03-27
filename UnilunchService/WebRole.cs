629507704f0a06fa3edc97a728b80386ed5b4310
Antti Niiles <antti.niiles@gmail.com>
2013-03-27 23:52:31 +0200

    disable diagnostics

--- UnilunchService/WebRole.cs
+++ UnilunchService/WebRole.cs
@@ -12,9 +12,9 @@ namespace UnilunchService
         public override bool OnStart()
         {
             // To enable the AzureLocalStorageTraceListner, uncomment relevent section in the web.config  
-            DiagnosticMonitorConfiguration diagnosticConfig = DiagnosticMonitor.GetDefaultInitialConfiguration();
-            diagnosticConfig.Directories.ScheduledTransferPeriod = TimeSpan.FromMinutes(1);
-            diagnosticConfig.Directories.DataSources.Add(AzureLocalStorageTraceListener.GetLogDirectory());
+            //DiagnosticMonitorConfiguration diagnosticConfig = DiagnosticMonitor.GetDefaultInitialConfiguration();
+            //diagnosticConfig.Directories.ScheduledTransferPeriod = TimeSpan.FromMinutes(1);
+            //diagnosticConfig.Directories.DataSources.Add(AzureLocalStorageTraceListener.GetLogDirectory());
 
             // For information on handling configuration changes
             // see the MSDN topic at http://go.microsoft.com/fwlink/?LinkId=166357.
