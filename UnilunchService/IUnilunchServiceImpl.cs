1b03446feccf9142716d03ac595217637cfcda4c
Antti Niiles <antti.niiles@gmail.com>
2013-03-27 17:10:14 +0200

    skeleton for web service added

---
+++ UnilunchService/IUnilunchServiceImpl.cs
@@ -0,0 +1,17 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Collections.Generic;
+namespace UnilunchService
+{
+    [ServiceContract]
+    public interface IUnilunchServiceImpl
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "unilunch/restaurants")]
+        Restaurant JsonData();
+    }
+}
---
+++ UnilunchService/UnilunchServiceImpl.svc.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+namespace UnilunchService
+{
+    public class UnilunchServiceImpl : IUnilunchServiceImpl
+    {
+        public Restaurant JsonData()
+        {
+            var r = new Restaurant();
+            return r;
+        }
+    }
+}
